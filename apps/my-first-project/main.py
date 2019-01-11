ac55adc9572e0f6420ec96af78c22746460eccb1
juref <jure.filipic@gmail.com>
2019-01-11 10:51:36 +0100

    first commit

---
+++ apps/my-first-project/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import webapp2
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        return self.response.write('Hello SmartNinja! Namestili ste app.')
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+], debug=True)
+
+# TODO - naredi nekaj #
